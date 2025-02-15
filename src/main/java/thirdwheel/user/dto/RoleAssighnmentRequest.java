502a835c220ebd2205cfef4c85125c31409643c8
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-02-15 22:02:26 +0100

    Added dto's

--- src/main/java/thirdwheel/user/dto/RoleAssighnmentRequest.java
+++ src/main/java/thirdwheel/user/dto/RoleAssighnmentRequest.java
@@ -1,4 +1,14 @@
 package thirdwheel.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoleAssighnmentRequest {
+    private String roleName;
 }
--- src/main/java/thirdwheel/user/dto/UserRegistrationRequest.java
+++ src/main/java/thirdwheel/user/dto/UserRegistrationRequest.java
@@ -1,4 +1,17 @@
 package thirdwheel.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationRequest {
+    private String Fname;
+    private String Lname;
+    private String email;
+    private String pwdHash;
 }
