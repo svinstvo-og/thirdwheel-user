88cd7e5c63bad051aaf99b09aaa67870b7df2174
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-03-22 16:46:57 +0100

    Added RefreshTokenService

--- src/main/java/thirdwheel/user/service/RefreshTokenService.java
+++ src/main/java/thirdwheel/user/service/RefreshTokenService.java
@@ -1,7 +1,17 @@
 package thirdwheel.user.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
 
 @Service
 public class RefreshTokenService {
@@ -13,7 +23,47 @@ public class RefreshTokenService {
         this.jwtKeyService = jwtKeyService;
     }
 
+    public String generateToken(String email) {
 
+        Key secretKey = jwtKeyService.getSecretKey();
 
+        return Jwts.builder()
+                .subject(email)
+                .id(UUID.randomUUID().toString()) // jti
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + 1000 * 60 * 60 * 24)) // 1 day
+                .signWith(secretKey)
+                .compact();
+    }
 
+    public String extractEmail(String token) {
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(jwtKeyService.getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractEmail(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
